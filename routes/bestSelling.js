325de8cc737f97cdcbf81a6c26873df6b8b555db
Himanshu <guptahimanshu479@gmail.com>
2019-09-15 23:49:26 +0530

    Bestselling likes feature

--- routes/bestSelling.js
+++ routes/bestSelling.js
@@ -41,7 +41,7 @@ router.post("/", middleware.checkIsAdmin, function (req, res) {
 
 //SHOW - show more info about one Best Selling book
 router.get("/:id", function (req, res) {
-    BestSelling.findById(req.params.id).populate("comments").exec(function (err, foundBestSelling) {
+    BestSelling.findById(req.params.id).populate("comments likes").exec(function (err, foundBestSelling) {
         if (err || !foundBestSelling) {
             req.flash("error", "Book Not Found")
             console.log(err)
@@ -51,6 +51,36 @@ router.get("/:id", function (req, res) {
     });
 });
 
+// BestSelling Like Route
+router.post("/:id/like", middleware.isLoggedIn, function (req, res) {
+    BestSelling.findById(req.params.id, function (err, foundBestSelling) {
+        if (err || !foundBestSelling) {
+            console.log(err);
+            req.flash("error", "Book Not Found!");
+            return res.redirect("/bestselling");
+        }
+        // check if req.user._id exists in foundBestSelling.likes
+        var foundUserLike = foundBestSelling.likes.some(function (like) {
+            return like.equals(req.user._id);
+        });
+
+        if (foundUserLike) {
+            // user already liked, removing like
+            foundBestSelling.likes.pull(req.user._id);
+        } else {
+            // adding the new user like
+            foundBestSelling.likes.push(req.user);
+        }
+
+        foundBestSelling.save(function (err) {
+            if (err) {
+                console.log(err);
+                return res.redirect("/bestselling");
+            }
+            return res.redirect("/bestselling/" + foundBestSelling._id);
+        });
+    });
+});
 
 //EDIT - edit best selling book(admin only)
 router.get("/:id/edit", middleware.checkIsAdmin, function (req, res) {
@@ -61,11 +91,26 @@ router.get("/:id/edit", middleware.checkIsAdmin, function (req, res) {
 
 //UPGRADE - upgrade the best selling book
 router.put("/:id", middleware.checkIsAdmin, function (req, res) {
-    BestSelling.findByIdAndUpdate(req.params.id, req.body.bs, function (err, upgradedBestSelling) {
+    BestSelling.findById(req.params.id, function (err, bestSelling) {
         if (err) {
-            res.redirect("/bestselling");
+            req.flash("error", err.message);
+            res.redirect("back");
         } else {
-            res.redirect("/bestselling/" + req.params.id);
+            bestSelling.title = req.body.bs.title;
+            bestSelling.price = req.body.bs.price;
+            bestSelling.author = req.body.bs.author;
+            bestSelling.image = req.body.bs.image;
+            bestSelling.description = req.body.bs.description;
+            bestSelling.publisher = req.body.bs.publisher;
+            bestSelling.save(function (err) {
+                if (err) {
+                    req.flash("error", err.message);
+                    res.redirect("back");
+                } else {
+                    req.flash("success", "Successfully Updated!");
+                    res.redirect("/bestselling/" + bestSelling._id);
+                }
+            });
         }
     });
 });
