448858f81ae2e3bf52646ca3bc7ebfe07ec1e6b0
Himanshu <guptahimanshu479@gmail.com>
2019-11-03 04:52:25 +0530

    Create new Reviews route and page

--- routes/reviews.js
+++ routes/reviews.js
@@ -18,5 +18,46 @@ router.get("/", function (req, res) {
     });
 });
 
+// Reviews New
+router.get("/new", middleware.isLoggedIn, middleware.checkReviewExistence, function (req, res) {
+    // middleware.checkReviewExistence checks if a user already reviewed the book, only one review per user is allowed
+    Book.findById(req.params.id, function (err, book) {
+        if (err) {
+            req.flash("error", err.message);
+            return res.redirect("back");
+        }
+        res.render("reviews/new", {book: book});
+    });
+});
+
+// Reviews Create
+router.post("/", middleware.isLoggedIn, middleware.checkReviewExistence, function (req, res) {
+    Book.findById(req.params.id).populate("reviews").exec(function (err, book) {
+        if (err) {
+            req.flash("error", err.message);
+            return res.redirect("back");
+        }
+        Review.create(req.body.review, function (err, review) {
+            if (err) {
+                req.flash("error", err.message);
+                return res.redirect("back");
+            }
+            //add user username/id and associated book to the review
+            review.user.id = req.user._id;
+            review.user.username = req.user.username;
+            review.book = book;
+            //save review
+            review.save();
+            book.reviews.push(review);
+            // calculate the new average review for the book
+            book.rating = calculateAverage(book.reviews);
+            //save book
+            book.save();
+            req.flash("success", "Your review has been successfully added.");
+            res.redirect('/books/' + book._id);
+        });
+    });
+});
+
 
 module.exports = router;
